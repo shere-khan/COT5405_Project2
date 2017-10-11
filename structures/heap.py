24921b9ec566ade1df3b098d73ff49e88eaea00c
Justin-PC\Justin <justin.barry.e>
2017-10-11 17:10:00 -0400

    restarting heap priority queue impl

--- structures/heap.py
+++ structures/heap.py
@@ -1,63 +1,19 @@
-class BinaryTree:
-    class Node:
-        def __init__(self, vid, x):
-            self.__id = vid
-            self.__data = x
-            self.__lchild = None
-            self.__rchild = None
-
-        def lchild(self):
-            return self.__lchild
-
-        def rchild(self):
-            return self.__rchild
-
-        def data(self):
-            return self.__data
-
-        def id(self):
-            return self.__id
-
-        def __hash__(self):
-            return hash(id(self))
-
-        def __repr__(self):
-            return "id: {} data: {}\nlchild: {} rchild: {}".format(self.__id, self.__data, self.__lchild.id(),
-                                                                   self.__rchild.id())
-
-        def __str__(self):
-            return "id: {} data: {}\nlchild: {} rchild: {}".format(self.__id, self.__data, self.__lchild.id(),
-                                                                   self.__rchild.id())
-
+class HeapPriorityQueue:
     def __init__(self):
-        self.__root = self.Node()
+        self.__data = []
 
+    def __parent(self, j):
+        (j - 1) // 2
 
-class TreeTool:
-    @staticmethod
-    def print_tree(t):
-        TreeTool.depth_first_traversal(t, lambda x: print(x))
+    def __left(self, j):
+        return 2 * j + 1
 
-    @staticmethod
-    def depth_first_traversal(t, f):
-        f(t.root)
-        if t.root.lchild():
-            TreeTool.__depth_first_traversal(t, t.root.lchild, f)
-        if t.root.rchild():
-            TreeTool.__depth_first_traversal(t, t.root.lchild, f)
+    def __right(self, j):
+        return 2 * j + 2
 
-    @staticmethod
-    def __depth_first_traversal(t, n, f):
-        f(n)
-        if t.root.lchild:
-            TreeTool.__depth_first_traversal(t, t.root.lchild, f)
-        if t.root.rchild:
-            TreeTool.__depth_first_traversal(t, t.root.lchild, f)
+    def __insert(self, i):
+        self.__data.append(i)
+        self.__heapify_up(i)
 
-
-class Heap:
-    def __init__(self):
-        t = BinaryTree()
-
-    def insert(self, val):
+    def __heapify_up(self, i):
         pass
