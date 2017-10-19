4ecf2b9ed979e65dbb9848af7062cc7886a625b7
Justin-PC\Justin <justin.barry.e>
2017-10-19 17:35:28 -0400

    created graph_test module to unit test the graph and graph tool class

---
+++ test/graph_test.py
@@ -0,0 +1,16 @@
+import random
+import string
+import unittest
+
+from structures import heap, graph
+
+
+class TestProblem2(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_insert(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
