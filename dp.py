a5c9921fbb1306ae54594e1254c40f4c99428773
Justin-PC\Justin <justin.barry.e>
2017-11-11 20:04:14 -0500

    alignment code almost fully tested

--- dp.py
+++ dp.py
@@ -1,6 +1,8 @@
 class RNA:
-    def __init__(self):
-        pass
+    class Pair:
+        def __init__(self, l, r):
+            self.l = l
+            self.r = r
 
     def secondary_structure(self, s):
         n = len(s)
@@ -16,38 +18,118 @@ class RNA:
                     l += 1
 
     def compute_opt(self, i, j, t):
-
         max(self.secondary_structure(i, j - 1), max())
+
+    def populate_base_no_pointer(self, M, m, n):
+        for i in range(1, n + 1):
+            for j in range(0, m + 1):
+                M.append([i]) if j == 0 else M[i].append(0)
+
+    def populate_base(self, M, m, n):
+        for i in range(0, n + 1):
+            for j in range(0, m + 1):
+                if i == 0 and j == 0:
+                    M.append([(i, None)])
+                elif i == 0 and j > 0:
+                    M[i].append((j, (i, j - 1, 'left')))
+                elif i > 0 and j == 0:
+                    M.append([(i, (i - 1, j, 'up'))])
+                else:
+                    M[i].append((0, None))
+
+    def alignment_no_pointer(self, s1, s2, f):
+        n = len(s1)
+        m = len(s2)
+        M = [[i for i in range(m + 1)]]
+        self.populate_base_no_pointer(M, m, n)
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                vals = [M[i - 1][j - 1] + f(s1[i - 1], s2[j - 1]),
+                        M[i - 1][j] + f("", s2[j - 1]),
+                        M[i][j - 1] + f(s1[i - 1], "")]
+                mynn = min(vals)
+                M[i][j] = mynn
+
+        return M
 
     def alignment(self, s1, s2, f):
         n = len(s1)
         m = len(s2)
-        M = [[i for i in range(m)]]
-        for i in range(1, n):
-            M[i] = [i]
+        M = []
+        self.populate_base(M, m, n)
 
-        for i in range(1, n):
-            for j in range(1, m):
-                vals = [self.a(M, i, j, f),
-                        self.b(M, i, j, f),
-                        self.c(M, i, j, f)]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                a = M[i - 1][j - 1][0] + f(s1[i - 1], s2[j - 1])
+                b = M[i - 1][j][0] + f("", s2[j - 1])
+                c = M[i][j - 1][0] + f(s1[i - 1], "")
+                vals = [a, b, c]
                 mynn = min(vals)
-                M[i, j] = (mynn, self.getparent(vals.index(mynn)))
+                M[i][j] = (mynn, self.getparent(vals.index(mynn), i, j))
 
-    def unpack_alignment(self):
-        pass
+        return M
 
-    def getparent(self, val):
-        pass
+    def getparent(self, val, i, j):
+        if val == 0:
+            return i - 1, j - 1, 'diag'
+        if val == 1:
+            return i - 1, j, 'up'
+        if val == 2:
+            return i, j - 1, 'left'
 
-    def a(self, M, i, j, f):
-        return M[i - 1, j - 1] + f(i, j)
+    def unpack_alignment(self, M, s1, s2, r1, r2):
+        if M is None:
+            return 'No alignment'
+        n = len(s1)
+        m = len(s2)
+        parent_info = M[n][m][1]
+        if parent_info is None:
+            return 'No alignment'
 
-    def b(self, M, i, j, f):
-        return M[i - 1, j] + f("", j)
+        parent_i = parent_info[0]
+        parent_j = parent_info[1]
+        res = self.getstring(s1, s2, parent_info[2], n, m)
+        r1 += res[0]
+        r2 += res[1]
 
-    def c(self, M, i, j, f):
-        return M[i, j - 1] + f(i, "")
+        r1, r2 = self.__unpack_alignment(M, parent_i, parent_j, s1, s2, r1, r2)
+        return r1, r2
+
+    def __unpack_alignment(self, M, i, j, s1, s2, r1, r2):
+        parent_info = M[i][j][1]
+        if parent_info is None:
+            return r1, r2
+        if parent_info[0] == parent_info[1] == 0:
+            if parent_info[2] == 'up':
+                r1 = s1[i - 1] + r1
+                r2 = "_" + r2
+                return r1, r2
+
+            r1 = "_" + r1
+            r2 = s2[j - 1] + r2
+            return r1, r2
+
+        res = self.getstring(s1, s2, parent_info[2], i, j)
+        r1 = res[0] + r1
+        r2 = res[1] + r2
+
+        parent_i = parent_info[0]
+        parent_j = parent_info[1]
+        parent = M[parent_i][parent_j][1]
+
+        r1, r2 = self.__unpack_alignment(M, parent_i, parent_j, s1, s2, r1, r2)
+        return r1, r2
+
+    def getstring(self, s1, s2, s, i, j):
+        if s == 'diag':
+            return s1[i - 1], s2[j - 1]
+        if s == 'up':
+            # return s1[i - 1], s2[j]
+            return s1[i - 1], "_"
+        if s == 'left':
+            # return s1[i], s2[j - 1]
+            return "_", s2[j - 1]
 
 
 class RNATool:
--- test/dptest.py
+++ test/dptest.py
@@ -1,13 +1,50 @@
+import dp
 import unittest
 
 
 class TestProblem2(unittest.TestCase):
     def setUp(self):
-        pass
+        self.dynprog = dp.RNA()
 
-    def test_insert(self):
-        pass
+    # def test_alignment_no_pointer(self):
+    #     s1 = 'AACG'
+    #     s2 = 'ACTG'
+    #
+    #     f = lambda x, y: 1 if y == "" or x == "" or x != y else 0
+    #
+    #     align = self.dynprog.alignment_no_pointer(s1, s2, f)
+    #
+    #     print(align[-1][-1])
 
+    def test_alignment(self):
+        # s1 = 'AACGD'
+        # s2 = 'ACTG'
+        # s1 = 'AACGDXXQQQY'
+        # s2 = 'ACTGQXCQY'
+
+        s1 = 'AGGCTATCACCTGACCTCCAGGCCGATGCCC'
+        s2 = 'TAGCTATCACGACCGCGGTCGATTTGCCCGAC'
+
+        f = lambda x, y: 1 if y == "" or x == "" or x != y else 2
+
+        align = self.dynprog.alignment(s1, s2, self.cost)
+
+        # print(align[-1][-1])
+        r1 = ""
+        r2 = ""
+        r1, r2 = self.dynprog.unpack_alignment(align, s1, s2, r1, r2)
+        #
+        print(r1)
+        print(r2)
+
+    @staticmethod
+    def cost(x, y):
+        if y == x == "":
+            return 1
+        if x != y:
+            return 2
+        if x == y:
+            return 0
 
 if __name__ == '__main__':
     unittest.main()
